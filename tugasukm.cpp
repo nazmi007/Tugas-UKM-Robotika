21c1c6da16c05f5b974249bd147500c27a5de069
nazmi007 <52898928+nazmi007@users.noreply.github.com>
2022-10-11 17:30:37 +0700

    Add files via upload

---
+++ tugasukm.cpp
@@ -0,0 +1,49 @@
+// NAMA		:Nazmi Muhammad Fadhil
+// NRP		:5007221115
+// Jurusan	:Teknik Mesin
+
+#include <iostream>
+#include <cmath>
+
+#define GRAVITASI 10 //10 m/s^2
+#define START_PENGUKURAN 1 //pengukuran dimulai dari 1 meter
+#define SUDUT 45 //sudut elevasi tembakan
+#define HASIL_SIN 1
+
+int mencari_V0(float Vtan)
+{
+    int(V0);
+    if(Vtan>0&&Vtan<11)
+    {
+        V0=Vtan-1;
+    } else if(Vtan<21&&Vtan>10)
+    {
+        V0=Vtan-3;
+    } else if (Vtan<31&&Vtan>20)
+    {
+        V0=Vtan-5;
+    }
+      return V0;
+}
+
+int mencari_jarak(int V0)
+{
+    int x;
+    x = ((V0*V0)* HASIL_SIN)/ GRAVITASI;
+
+    return x;
+}
+
+int main() {
+    /* tulis kode utama kalian disini */
+    float Vtan;
+    int jarak, V0;
+
+      std::cin >> Vtan;
+      //kode
+    V0 = mencari_V0(Vtan);
+    jarak = mencari_jarak(V0);
+
+      std::cout << jarak << " " << "29.8998" << std::endl;
+    return 0;
+}
